5e1dc17603562cb585f8f46b7ff453f771bbd028
YevheniiGrabina <89870811+YevheniiGrabina@users.noreply.github.com>
2022-06-01 15:29:28 +0300

    Task3 done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,9 @@
 import Profile from './Profile/Profile';
 import Statistic from './Statistics/Statistic';
+import FriendList from './FriendList/FriendList';
 import user from 'user.json';
 import data from 'data.json';
+import friendsData from 'friends.json';
 
 
 export default function App() {
@@ -10,6 +12,7 @@ export default function App() {
       style={{
         height: '200vh',
         display: 'flex',
+          flexDirection: 'column',
         justifyContent: 'center',
         alignItems: 'center',
         fontSize: 40,
@@ -23,7 +26,8 @@ export default function App() {
         avatar={user.avatar}
         stats={user.stats}
       />
-        <Statistic title="Upload stats" stats={data} />
+      <Statistic title="Upload stats" stats={data} />
+      <FriendList friends={friendsData} />
     </div>
   );
 }
